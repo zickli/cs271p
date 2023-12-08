88206db8e5ba1f599deae0e8525d96c0b44a5752
zickli <lizijie424@gmail.com>
2023-12-07 16:44:04 -0800

    implement MST as the heuristic function of bnb-dfs

--- bnb_dfs.py
+++ bnb_dfs.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Path(list):
     def __init__(self, *args, cost=0.0, graph=None, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,11 +23,38 @@ class BranchAndBoundDfs:
         self.g = graph
         self.n = len(graph)
         self.best_path = Path([], cost=float('inf'))
+        self.stat = 0
 
-    def estimate_cost(self, path: Path, domain: set):
-        return path.cost
+    def minimum_spanning_tree(self, path: Path):
+        start_node = path[0]
+        visited = set(path)
+        edges = []
+        for to, weight in enumerate(self.g[start_node]):
+            if to != start_node:
+                edges.append((weight, start_node, to))
+        heapq.heapify(edges)
+
+        mst_cost = 0.0
+
+        while edges:
+            weight, frm, to = heapq.heappop(edges)
+            if to not in visited:
+                visited.add(to)
+                mst_cost += weight
+
+                for next_to, next_weight in enumerate(self.g[to]):
+                    if to == next_to:
+                        continue
+                    if next_to not in visited:
+                        heapq.heappush(edges, (next_weight, to, next_to))
+
+        return mst_cost
+
+    def estimate_cost(self, path: Path):
+        return path.cost + self.minimum_spanning_tree(path)
 
     def solve(self, path: Path, domain: set):
+        self.stat += 1
         if len(path) == self.n:
             path.append(path[0])
             path.cost += self.g[path[-1]][path[0]]
@@ -40,7 +70,7 @@ class BranchAndBoundDfs:
             new_domain = domain.copy()
             new_domain.remove(next_city)
 
-            new_paths.append((self.estimate_cost(new_path, new_domain),
+            new_paths.append((self.estimate_cost(new_path),
                               new_path,
                               new_domain))
 
@@ -66,4 +96,4 @@ if __name__ == '__main__':
     g = read_graph("10_0.0_1.0.out")
     solution = BranchAndBoundDfs(g)
     solution.solve(Path([], graph=g), {x for x in range(0, len(g))})
-    print(solution.best_path)
+    print(solution.best_path, solution.stat)
